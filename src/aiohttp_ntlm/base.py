0f784d400d7d11f21a3c356caa5a1a004254316e
ycqian <ycqian@hillstonenet.com>
2025-07-02 13:21:45 +0800

    Add type annotations

--- src/aiohttp_ntlm/base.py
+++ src/aiohttp_ntlm/base.py
@@ -9,16 +9,16 @@ class HttpNtlmAuthMiddleware:
         self.username: str = username
         self.password: str = password
 
-        self.auth_type = auth_type
-        self.auth_strip = self.auth_type + " "
+        self.auth_type: str = auth_type
+        self.auth_strip: str = self.auth_type + " "
 
     async def __call__(self, req: ClientRequest, handler: ClientHandlerType) -> ClientResponse:
-        resp = await handler(req)
+        resp: ClientResponse = await handler(req)
 
         if resp.status == 401:
-            spnego_options = spnego.NegotiateOptions.use_ntlm
-            target_hostname = req.headers["Host"]
-            client = spnego.client(
+            spnego_options: spnego.NegotiateOptions = spnego.NegotiateOptions.use_ntlm
+            target_hostname: str = req.headers["Host"]
+            client: spnego.ContextProxy = spnego.client(
                 self.username,
                 self.password,
                 protocol="ntlm",
@@ -27,17 +27,17 @@ class HttpNtlmAuthMiddleware:
                 service="http",
                 options=spnego_options,
             )
-            negotiate_message = base64.b64encode(client.step() or b"").decode()
-            auth = f"NTLM {negotiate_message}"
+            negotiate_message: str = base64.b64encode(client.step() or b"").decode()
+            auth: str = f"NTLM {negotiate_message}"
             req.headers["Authorization"] = auth
 
             resp = await handler(req)
 
             # get the challenge
-            auth_header_value = resp.headers["WWW-Authenticate"]
+            auth_header_value: str = resp.headers["WWW-Authenticate"]
 
-            val = base64.b64decode(auth_header_value[len(self.auth_strip) :].encode())
-            authenticate_message = base64.b64encode(client.step(val) or b"").decode()
+            val: bytes = base64.b64decode(auth_header_value[len(self.auth_strip) :].encode())
+            authenticate_message: str = base64.b64encode(client.step(val) or b"").decode()
             auth = f"{self.auth_type} {authenticate_message}"
             req.headers["Authorization"] = auth
             resp = await handler(req)
